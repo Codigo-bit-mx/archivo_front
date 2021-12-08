05bd32a728c52b1bb676e8607e8a1e9f5379b5f9
Ossmar <codigobitmx@gmail.com>
2021-12-08 13:12:18 -0600

    arreglo en env

--- helpers/upload-aws.js
+++ helpers/upload-aws.js
@@ -1,9 +1,9 @@
 import S3 from 'aws-sdk/clients/s3';
 import fs from 'fs';
 
-const region          = process.env.AWS_REGION; 
-const accessKeyId     = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const region          = process.env.REGION; 
+const accessKeyId     = process.env.ACCESS_KEY_ID;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY;
 
 
 const storage = new S3({
