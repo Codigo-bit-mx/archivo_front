c458a563e8a9873176bcfed69eac5de0b266e11b
Ossmar González [MEX] <ogonzalez@compusoluciones.com>
2023-03-28 14:23:11 -0600

    prueba6

--- components/Layout.js
+++ components/Layout.js
@@ -19,9 +19,7 @@ const Layout = ({children}) => {
             <title> DropBIT !! </title>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" /> 
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />"
-            <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow&display=swap" rel="stylesheet"></link>
+            
 
         </DocumentHead>
 
